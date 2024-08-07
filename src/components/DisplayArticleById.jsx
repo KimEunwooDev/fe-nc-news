119dad60afd01e6948fd54fa12faa9dac32434bf
Eunwoo Kim <153867711+KimEunwooDev@users.noreply.github.com>
2024-08-07 12:28:23 +0100

    add isLoading state to show message during loading data

--- src/components/DisplayArticleById.jsx
+++ src/components/DisplayArticleById.jsx
@@ -2,28 +2,34 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import SingleArticle from "./SingleArticle";
+import CommentsProvider from "./CommentsProvider";
 
 const DisplayArticleById = () => {
   const { article_id } = useParams();
   const [currentArticle, setCurrentArticle] = useState({});
-  const [isLoading, setIsLoading] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get(`https://eunwoo-nc-news.onrender.com/api/articles/${article_id}`)
       .then((response) => {
-        console.log(response.data, "response in DAB");
         const { article } = response.data;
         setCurrentArticle(article);
+        setIsLoading(false);
       })
       .catch((err) => {
         return "error!";
       });
   }, [article_id]);
 
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <>
       <SingleArticle currentArticle={currentArticle} />
+      <CommentsProvider currentArticle={currentArticle} />
     </>
   );
 };
