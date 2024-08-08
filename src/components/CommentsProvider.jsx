caa7b81bf14ca4f5f4004631e14833d5d8662f0f
Eunwoo Kim <153867711+KimEunwooDev@users.noreply.github.com>
2024-08-08 15:31:41 +0100

    add component for adding comment when user add commnet

--- src/components/CommentsProvider.jsx
+++ src/components/CommentsProvider.jsx
@@ -1,15 +1,19 @@
 import CommentsList from "./CommentsList";
+import CommentAdder from "./CommentAdder";
 import { useState } from "react";
 
 const CommentsProvider = ({ currentArticle }) => {
   const articleId = currentArticle.article_id;
   const [commentsById, setCommentsById] = useState([]);
   return (
-    <CommentsList
-      articleId={articleId}
-      commentsById={commentsById}
-      setCommentsById={setCommentsById}
-    />
+    <>
+      <CommentsList
+        articleId={articleId}
+        commentsById={commentsById}
+        setCommentsById={setCommentsById}
+      />
+      <CommentAdder articleId={articleId} setCommentsById={setCommentsById} />
+    </>
   );
 };
 
