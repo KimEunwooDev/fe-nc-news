9589404199b85619616547e9cb9cacc839d32234
Eunwoo Kim <153867711+KimEunwooDev@users.noreply.github.com>
2024-08-09 12:53:25 +0100

    add searchParams props

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 import DisplayArticles from "./DisplayArticles";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
-  return <DisplayArticles />;
+  const [searchParams, setSearchParams] = useSearchParams();
+  return (
+    <DisplayArticles
+      searchParams={searchParams}
+      setSearchParams={setSearchParams}
+    />
+  );
 };
 
 export default Home;
